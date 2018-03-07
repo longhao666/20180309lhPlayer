967f5c997739846e2293f35b444da0d94f76c6c3
longhao666 <305902464@qq.com>
2018-03-07 10:04:59 +0800

    还是想先自己写一个播放器,不行再用这个

--- player/main.cpp
+++ player/main.cpp
@@ -1,11 +1,38 @@
 #include "player.h"
 #include <QApplication>
 
+#include <QCommandLineParser>
+#include <QCommandLineOption>
+#include <QDir>
+
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    Player w;
-    w.show();
+    QApplication app(argc, argv);
 
-    return a.exec();
+    QCoreApplication::setApplicationName("Player Example");
+    QCoreApplication::setOrganizationName("QtProject");
+    QCoreApplication::setApplicationVersion(QT_VERSION_STR);
+    QCommandLineParser parser;
+    parser.setApplicationDescription("Qt MultiMedia Player Example");
+    parser.addHelpOption();
+    parser.addVersionOption();
+    parser.addPositionalArgument("url", "The URL to open.");
+    parser.process(app);
+
+    Player player;
+
+    if (!parser.positionalArguments().isEmpty() && player.isPlayerAvailable()) {
+        QList<QUrl> urls;
+        foreach (const QString &a, parser.positionalArguments())
+            urls.append(QUrl::fromUserInput(a, QDir::currentPath(), QUrl::AssumeLocalFile));
+        player.addToPlaylist(urls);
+    }
+
+#if defined(Q_WS_SIMULATOR)
+    player.setAttribute(Qt::WA_LockLandscapeOrientation);
+    player.showMaximized();
+#else
+    player.show();
+#endif
+    return app.exec();
 }
